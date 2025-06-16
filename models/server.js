103d6a2b955376c092b7da0f353c80ce272f8598
Fer <lgutierrez.dscont@gmail.com>
2025-06-16 15:48:43 -0500

    primer commit

---
+++ models/server.js
@@ -0,0 +1,41 @@
+//Servidor de Express
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+
+class Server {
+   constructor() {
+      this.app = express();
+      this.port = process.env.PORT;
+
+      //Http server
+      this.server = http.createServer(this.app);
+
+      //configuracion de sockets
+      this.io = socketio(this.server, {});
+   }
+
+   middleware() {
+      this.app.use(express.static(path.resolve(__dirname, "../public")));
+   }
+
+   configurarSockets() {
+      new Sockets(this.io);
+   }
+   execute() {
+      //Inicializar Middleware
+      this.middleware();
+
+      //Inicializar Middleware
+      this.configurarSockets();
+
+      //Inicializar Server
+      this.server.listen(this.port, () => {
+         console.log("Server corriendo en puerto :", this.port);
+      });
+   }
+}
+
+module.exports = Server;
